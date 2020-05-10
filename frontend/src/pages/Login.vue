31bc4bb1af4389252a3853c54e2d0272979a2347
Aidan <akinzett@gmail.com>
2020-05-10 23:05:14 +1000

    login page

---
+++ frontend/src/pages/Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-page class="column justify-center items-center">
+    <q-card :style="{ padding: '10px', width: '400px', marginBottom: '50px' }">
+      <q-card-section>
+        <div class="text-h5">Login</div>
+      </q-card-section>
+      <q-card-section>
+        <q-input v-model="text" label="Username" />
+        <q-input v-model="password" type="password" label="Password" />
+      </q-card-section>
+      <q-seperator />
+      <q-card-actions>
+        <q-btn flat>Login</q-btn>
+      </q-card-actions>
+    </q-card>
+  </q-page>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'PageIndex',
+  components: {},
+  data() {
+    return {
+      text: '',
+      password: ''
+    };
+  }
+};
+</script>
+
+<style></style>
